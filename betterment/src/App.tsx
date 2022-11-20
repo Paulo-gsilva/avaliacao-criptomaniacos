8cc8dda129f551a65dabcbe08306e428111617d6
Paulo Gonçalves <guigui041201@gmail.com>
2022-11-19 21:08:51 -0300

    Adiciona TypeScript ao projeto
    - Modifica componentes
    - Adiciona dependências

---
+++ betterment/src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import InitialPage from "./Components/InitialPage/InitialPage";
+
+function App() {
+  return <InitialPage />;
+}
+
+export default App;
---
+++ betterment/src/Components/InitialPage/InitialPage.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+function InitialPage() {
+  return <h1>initialpage</h1>;
+}
+
+export default InitialPage;
